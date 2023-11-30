443bb84e6eb173f53c8e3b88e695825f0cb13c3e
mattiamaffo <126592486+mattiamaffo@users.noreply.github.com>
2023-11-30 14:20:20 +0100

    Update hw_dt1.py

--- hw_dt1.py
+++ hw_dt1.py
@@ -1,7 +1,9 @@
 import numpy as np
 import seaborn as sns
+from statistics import mean
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import normalize
 import load_data as ld
 from sklearn.svm import SVC
 from sklearn.linear_model import Perceptron
@@ -24,6 +26,7 @@ def svm_model(X_train, Y_train, x_test, y_test):
     # }
     # randomized_search = RandomizedSearchCV(model, random_param, cv=5, scoring="accuracy", verbose=1, n_jobs=-1, n_iter = 10)
     # randomized_search.fit(X_train, Y_train)
+
     # #PRINT THE BEST PARAMETERS FOUND
     # print("Best parameters found:", randomized_search.best_params_) #C: 1.6, degree: 3, kernel: rbf
 
@@ -32,7 +35,7 @@ def svm_model(X_train, Y_train, x_test, y_test):
     # C = randomized_search.best_params_['C']
     # degree = randomized_search.best_params_['degree']
 
-    model = SVC(kernel='rbf', C=1.6)  # FOR THE REPORT, SHOW ALSO THE RESULTS WITH DIFFERENT HYPER-P.
+    model = SVC(kernel='sigmoid', C=1.2, gamma='scale')  # FOR THE REPORT, SHOW ALSO THE RESULTS WITH DIFFERENT HYPER-P.
 
     # FIT THE MODEL
     model.fit(X_train, Y_train)
@@ -48,28 +51,17 @@ def svm_model(X_train, Y_train, x_test, y_test):
 
     precision = precision_score(y_test, y_pred, average=None)
     recall = recall_score(y_test, y_pred, average=None)
-    print("Precision of SVM model: \n", precision)
-    print("Recall of SVM model: \n", recall)
+    print("Precision of SVM model (sigmoid, C = 1.2): \n", precision)
+    print("Precision mean of SVM model (sigmoid, C = 1.2): \n", round(precision.mean(), 5))
+    print("Recall of SVM model (sigmoid, C = 1.2): \n", recall)
+    print("Recall mean of SVM model (sigmoid, C = 1.2): \n", round(recall.mean(), 5))
 
     f1 = f1_score(y_test, y_pred, average=None)
-    print("F1_score of SVM model: \n", f1)
+    print("F1_score of SVM model (sigmoid, C = 1.2): \n", f1)
+    print("F1_score mean of SVM model (sigmoid, C = 1.2): \n", round(f1.mean(), 5))
 
     conf_matrix = confusion_matrix(y_test, y_pred)
-    print("Confusion Matrix of SVM model: \n", conf_matrix)
-
-    # PLOT RESULTS:
-    # --- PRECISION-RECALL CURVE
-    # precision_d = dict()
-    # recall_d = dict()
-    # for i in range(len(unique_class)):
-    #     precision_d[i], recall_d[i], _ = precision_recall_curve(y_test == unique_class[i], y_pred == unique_class[i])
-    #     plt.plot(recall_d[i], precision_d[i], lw=2, label='class {}'.format(unique_class[i]))
-    #
-    # plt.xlabel("Recall")
-    # plt.ylabel("Precision")
-    # plt.legend(loc="best")
-    # plt.title("Precision-Recall Curve")
-    # plt.show()
+    print("Confusion Matrix of SVM model (sigmoid, C = 1.2): \n", conf_matrix)
 
     sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=['0', '1','2','3','4','5','6','7','8','9'], yticklabels=['0', '1','2','3','4','5','6','7','8','9'])
     plt.xlabel('Predicted Values')
@@ -78,24 +70,9 @@ def svm_model(X_train, Y_train, x_test, y_test):
     plt.savefig('matrix_confusion_svm.png')
     plt.show()
 
-    # --- ROC CURVE:
-    # fpr = dict()
-    # tpr = dict()
-    # roc_auc = dict()
-    # for i in range(len(unique_class)):
-    #     fpr[i], tpr[i], _ = roc_curve(y_test == unique_class[i], y_pred == unique_class[i])
-    #     roc_auc[i] = auc(fpr[i], tpr[i])
-    #     plt.plot(fpr[i], tpr[i], lw=2, label='class {} (AUC = {:.2f})'.format(unique_class[i], roc_auc[i]))
-    #
-    # plt.xlabel("False Positive Rate")
-    # plt.ylabel("True Positive Rate")
-    # plt.legend(loc="best")
-    # plt.title("ROC Curve")
-    # plt.show()
-
-    return accuracy
 def perceptron_model(X_train, Y_train, x_test, y_test):
-    # FIND BEST PARAMETERS WITH GRIDSEARCH:
+
+    # FIND BEST PARAMETERS WITH RANDOMIZED-SEARCH:
     model = Perceptron()
     random_param = {
         'penalty': ['l2', 'l1', 'elasticnet'],
@@ -105,8 +82,10 @@ def perceptron_model(X_train, Y_train, x_test, y_test):
     }
     randomized_search = RandomizedSearchCV(model, random_param, cv=5, scoring="accuracy", verbose=1, n_jobs=-1, n_iter=10)
     randomized_search.fit(X_train, Y_train)
+
     #PRINT THE BEST PARAMETERS FOUND
     print("Best parameters found:", randomized_search.best_params_) #eta0 = 0.01, n_iter = 700, penalty = l1, alpha = 1e-05
+
     # SET PARAMETERS
     print(X_train.shape)
     n_iter = randomized_search.best_params_['max_iter']
@@ -130,10 +109,13 @@ def perceptron_model(X_train, Y_train, x_test, y_test):
     precision = precision_score(y_test, y_pred, average=None)
     recall = recall_score(y_test, y_pred, average=None)
     print("Precision of Perceptron model: \n", precision)
+    print("Precision mean of Perceptron model: \n", round(precision.mean(), 5))
     print("Recall of Perceptron model: \n", recall)
+    print("Recall mean of Perceptron model: \n", round(recall.mean(), 5))
 
     f1 = f1_score(y_test, y_pred, average=None)
     print("F1_score of Perceptron model: \n", f1)
+    print("F1_score mean of Perceptron model: \n", round(f1.mean(), 5))
 
     conf_matrix = confusion_matrix(y_test, y_pred)
     print("Confusion Matrix of Decision Tree model: \n", conf_matrix)
@@ -146,8 +128,6 @@ def perceptron_model(X_train, Y_train, x_test, y_test):
     plt.title('Confusion Matrix Perceptron')
     plt.savefig('matrix_confusion_perceptron.png')
     plt.show()
-
-    return accuracy
 def decision_tree_model(X_train, Y_train, x_test, y_test):
     model = tree.DecisionTreeClassifier()
 
@@ -164,10 +144,13 @@ def decision_tree_model(X_train, Y_train, x_test, y_test):
     precision = precision_score(y_test, y_pred, average=None)
     recall = recall_score(y_test, y_pred, average=None)
     print("Precision of Decision Tree model: \n", precision)
+    print("Precision mean of Decision Tree: \n", round(precision.mean(),5))
     print("Recall of Decision Tree model: \n", recall)
+    print("Recall mean of Decision Tree: \n", round(recall.mean(), 5))
 
     f1 = f1_score(y_test, y_pred, average=None)
     print("F1_score of Decision Tree model: \n", f1)
+    print("F1_score mean of Decision Tree: \n", round(f1.mean(),5))
 
 
     conf_matrix = confusion_matrix(y_test, y_pred)
@@ -182,30 +165,28 @@ def decision_tree_model(X_train, Y_train, x_test, y_test):
     plt.savefig('matrix_confusion_dt.png')
     plt.show()
 
-    return accuracy
-
 
 # LOAD THE DATASET
 X, Y = ld.load_data('dataset1.csv')
 x_data = np.array(X)
 y = np.array(Y)
-accuracies = [] #accuracies[0] = first_model, accuracies[1] = second_model, ecc..
 print("Shape of x:", x_data.shape)
 print("Shape of y:", y.shape)
 
-pre_p = input("Do you want to pre-process X? Type y/n.\n")
-while pre_p != "y" and pre_p != "Y" and pre_p != "n" and pre_p != "N":
+pre_p = input("Do you want to pre-process X with Standard Scaler or Normalize? Type s/n.\n")
+while pre_p != "s" and pre_p != "S" and pre_p != "n" and pre_p != "N":
     print("This input is not valid!")
-    pre_p = input("Do you want to pre-process X? Type y/n.\n")
-if pre_p == "y" or pre_p == "Y":
+    pre_p = input("Do you want to pre-process X with Standard Scaler or Normalize? Type s/n.\n")
+if pre_p == "s" or pre_p == "S":
     # PRE-PROCESSING X
     scaler = StandardScaler()
     x_data = scaler.fit_transform(x_data)
-    print("Data pre-processed: \n")
+    print("Data pre-processed with Standard Scaler: \n")
     print(x_data)
     pre_p = True
 elif pre_p == "n" or pre_p == "N":
-    print("Data NOT pre-processed: \n")
+    x_data = normalize(x_data, norm='l2')
+    print("Data pre-processed with Normalize: \n")
     print(x_data)
     pre_p = False
 
@@ -214,11 +195,11 @@ X_train, x_test, Y_train, y_test = train_test_split(x_data, y, test_size= 0.2, r
 
 #TEST THE MODELS:
 print("Run SVM...\n")
-accuracy1 = svm_model(X_train, Y_train, x_test, y_test)
+svm_model(X_train, Y_train, x_test, y_test)
 print("Run Perceptron...\n")
-accuracy2 = perceptron_model(X_train, Y_train, x_test, y_test)
+perceptron_model(X_train, Y_train, x_test, y_test)
 print("Run Decision Tree...\n")
-accuracy3 = decision_tree_model(X_train, Y_train, x_test, y_test)
+ecision_tree_model(X_train, Y_train, x_test, y_test)
 
 
 
